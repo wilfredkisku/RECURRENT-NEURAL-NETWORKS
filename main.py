c950a32067219df40ff47fa8b2042efee1d8141d
wilfredkisku <wilfred.kisku@gmail.com>
2021-04-14 02:19:38 +0530

    changes in main

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from nltk.tokenize import word_tokenize, RegexpTokenizer, WordPunctTokenizer
 from nltk.probability import FreqDist
 from datetime import datetime
 from utils import *
+#from sklearn.utils.extmath import softmax
 import matplotlib.pyplot as plt
 
 ##define the variables 
@@ -81,5 +82,65 @@ print("Eamples : %s"%tokenized_sentence_new[14999])
 X_train = np.asarray([[vocab_words_index[w] for w in sen[:-1]] for sen in tokenized_sentence_new])
 Y_train = np.asarray([[vocab_words_index[w] for w in sen[1:]] for sen in tokenized_sentence_new])
 
-print(X_train[14999])
+#print(X_train[14999])
+#print(Y_train[14999])
 
+####################
+##Part3
+
+class RNNnumpy:
+    def __init__(self, word_dim, hidden_dim = 100, bptt_truncate = 4):
+        self.word_dim = word_dim
+        self.hidden_dim = hidden_dim
+        self.bptt_truncate = bptt_truncate
+
+        self.U = np.random.uniform(-np.sqrt(1./word_dim), np.sqrt(1./word_dim), (hidden_dim, word_dim))
+        self.V = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (word_dim, hidden_dim))
+        self.W = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (hidden_dim, hidden_dim))
+    
+    def softmax(self, x):
+        return np.exp(x) / np.exp(x).sum(axis=0)
+
+    def forward_propagation(self, x):
+        #where x is the input sentence sampled from the dataset (0 to t-1)
+        T = len(x)
+        s = np.zeros((T+1, self.hidden_dim))
+        #print(s.shape)
+        s[-1] = np.zeros(self.hidden_dim)
+        #print(s.shape)
+        
+        o = np.zeros((T, self.word_dim))
+
+        for t in np.arange(T):
+            s[t] = np.tanh(self.U[:,x[t]] + self.W.dot(s[t-1]))
+            o[t] = self.softmax(self.V.dot(s[t]))
+        return [o, s]
+    
+    def predict(self, x):
+        o, s = self.forward_propagation(x)
+        return np.argmax(o, axis=1)
+    
+    def calculate_total_loss(self, x, y):
+        L = 0
+        for i in np.arange(len(y)):
+            o, s = self.forward_propagation(x[i])
+            correct_word_predictions = o[np.arange(len(y[i])), y[i]]
+            L += -1 * np.sum(np.log(correct_word_predictions))
+        return L
+
+    def calculate_loss(self, x, y):
+        N = np.sum((len(y_i) for y_i in y))
+        return self.calculate_total_loss(x, y)/N
+
+obj = RNNnumpy(vocabulary_size)
+o, s = obj.forward_propagation(X_train[10])
+print(len(X_train[10]))
+print(o.shape)
+print(o)
+
+predictions = obj.predict(X_train[10])
+print(predictions.shape)
+print(predictions)
+
+words = [list(vocab_words_index.keys())[list(vocab_words_index.values()).index(p)] for p in predictions]
+print(words)
