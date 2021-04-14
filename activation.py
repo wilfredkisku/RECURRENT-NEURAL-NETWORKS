9cc1d820ae42492da4802487a363a9f3f75aa8ed
wilfredkisku <wilfred.kisku@gmail.com>
2021-04-15 01:16:58 +0530

    new files

---
+++ activation.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class sigmoid:
+    def forward(self, x):
+        return 1.0 / (1.0 + np.exp(-x))
+
+    def backward(self, x, top_diff):
+        output = self.forward(x)
+        return (1.0 - output) * output * top_diff
+
+class tanh:
+    def forward(self, x):
+        return np.tanh(x)
+
+    def backward(self, x, top_diff):
+        output = self.forward(x)
+        return (1.0 - np.square(output))*top_diff
+
